19189a50f8c727110b35f988bc742483352ef6c7
shellbye <shellbye.com@gmail.com>
2020-03-31 12:28:27 +0800

    1365 v2v3

--- 1365.go
+++ 1365.go
@@ -17,7 +17,46 @@ func smallerNumbersThanCurrent(nums []int) []int {
 	return ret
 }
 
+func smallerNumbersThanCurrentV2(nums []int) []int {
+	var buckets [101]int
+	for i := 0; i < len(nums); i++ {
+		buckets[nums[i]] += 1
+	}
+
+	var sums [101]int
+	sums[0] = 0
+	for i := 1; i < len(sums); i++ {
+		sums[i] = sums[i-1] + buckets[i-1]
+	}
+
+	var ret = make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		ret[i] = sums[nums[i]]
+	}
+
+	return ret
+}
+
+func smallerNumbersThanCurrentV3(nums []int) []int {
+	var buckets [101]int
+	for i := 0; i < len(nums); i++ {
+		buckets[nums[i]] += 1
+	}
+
+	pre := 0
+	for i := 0; i < len(buckets); i++ {
+		buckets[i], pre = pre, buckets[i]+pre
+	}
+
+	var ret = make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		ret[i] = buckets[nums[i]]
+	}
+
+	return ret
+}
+
 func main() {
-	nums := []int{3, 2, 4}
-	fmt.Println(smallerNumbersThanCurrent(nums))
+	nums := []int{3, 2, 0, 4}
+	fmt.Println(smallerNumbersThanCurrentV3(nums))
 }
